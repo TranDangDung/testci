80323dd98618ea51fb4d8319c678dfc0be309e84
@dung.td <dung.td@teko.vn>
2024-01-18 00:07:33 +0700

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
-      ) ,
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
