4370e501b2b9cfd1da56e433fd7fcacd1527878f
@dung.td <dung.td@teko.vn>
2024-01-18 00:03:28 +0700

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -30,11 +30,11 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage>  {
+class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
-    setState((){
+    setState(() {
       _counter++;
     });
   }
