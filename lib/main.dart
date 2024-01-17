26ee61bd6b21e49768756813796ee6b803f124b8
@dung.td <dung.td@teko.vn>
2024-01-17 23:54:51 +0700

    Update ui to bug

--- lib/main.dart
+++ lib/main.dart
@@ -30,11 +30,11 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>  {
   int _counter = 0;
 
   void _incrementCounter() {
-    setState(() {
+    setState((){
       _counter++;
     });
   }
@@ -45,7 +45,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
-      ),
+      ) ,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
